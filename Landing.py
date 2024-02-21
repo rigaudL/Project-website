02eec52fb6910fde2fd00483d60bcf7c66bd5b7d
Rigaud Luly <RigaudLuly21@gmail.com>
2024-02-20 20:24:46 -0500

    next commit

--- Landing.py
+++ Landing.py
@@ -1,28 +1,17 @@
-# import streamlit as st
-
-# st.set_page_config("Wide")
-# st.title("Welcome to my Project hosting page")
-# first , second = st.columns(2)
-
-# with first:
-#     st.camera_input(label= "camera")
-
-# with second:
-#     st.text("HEllo i am Rigaud Luly and welcome to my website where i host \
-#         all my pojects and you can contact me")
-
 import streamlit as st
-text = "HEllo i am Rigaud Luly and welcome to my website where i host \
-        all my pojects and you can contact me"
-st.set_page_config(layout="wide")
+st.set_page_config(layout="wide") 
 st.title("Welcome to my Project hosting page")
 first , second = st.columns(2)
 
 with first:
-    st.camera_input(label= "camera")
+    #st.camera_input(label= "camera")
+    st.image("images/download (2).jpg") # takes a img as the parameter and put it on the screen and make it an obj of "st.image()"
 
 with second:
-    st.text(text)
+    text = "Hello I am Rigaud Luly and welcome to my website where I host \
+            all my pojects and you can contact me."
+    st.title("Rigaud Luly")
+    st.write(text) # whats st.text()
     
 st.text_input(label="", placeholder="My message" )
 
