451e29aa9f5e2965a92f5b4c95a1fd835081988d
Rigaud Luly <RigaudLuly21@gmail.com>
2024-02-24 15:23:37 -0500

    adding a form

--- pages/Contact.py
+++ pages/Contact.py
@@ -2,3 +2,17 @@ import streamlit as st
 
 st.title("Contact me")
 
+with st.form(key="email"): #component that has other stuff inside like st.colum and must have a key inside (id for the component)
+    user_email = st.text_input('your email addresss')
+    
+    st.info("Type Your message below")
+    user_message = st.text_area("")
+    
+    
+    button       = st.form_submit_button("SUBMIT")
+    # st.form_submit_button is a boolean and returnd true or false values
+    
+    if button:
+        print("The user has submitted the message \n")
+        print(user_message)
+        
