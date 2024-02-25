eff091ea3ce75af818c57e955f0192718cc0617e
Rigaud Luly <RigaudLuly21@gmail.com>
2024-02-25 11:13:44 -0500

    ok

--- contact.py
+++ contact.py
@@ -1,3 +1,14 @@
-import streamlit as st
+import smtplib, ssl
 
-st.title("Contact me")
+# smtplib Is used to send emails
+#ssl is used to keep things 
+host = "smtp.gmail.com"
+
+username = "Rigaudluly21@gmail.com"
+password = "nrqy fixw psbv kmby"
+
+context = ssl.create_default_context()
+
+with smtplib.SMTP_SSL(host, port , context= context) as server:
+    server.login()
+    server.sendmail()
