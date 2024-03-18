a02c945f3b57f1ffa43e82339cbd3b24babc5ce9
Rigaud Luly <RigaudLuly21@gmail.com>
2024-03-18 01:41:47 -0400

    connecting backend to my front end contact page

--- contact.py
+++ contact.py
@@ -3,12 +3,21 @@ import smtplib, ssl
 # smtplib Is used to send emails
 #ssl is used to keep things 
 host = "smtp.gmail.com"
-
+port = 465
 username = "Rigaudluly21@gmail.com"
-password = "nrqy fixw psbv kmby"
+password = "nrqyfixwpsbvkmby"
+
+receiver = "Rigaudlulynil@gmail.com"
 
 context = ssl.create_default_context()
+message = """
+Hi how are you
+
+ok?
+
+
+"""
 
 with smtplib.SMTP_SSL(host, port , context= context) as server:
-    server.login()
-    server.sendmail()
+    server.login(username, password)
+    server.sendmail(username, receiver, message )
