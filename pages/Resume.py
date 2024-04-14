b893284a9704ddb7403d628867d1cfb1740535af
Rigaud Luly <RigaudLuly21@gmail.com>
2024-04-13 23:33:45 -0400

    FIxed resume image

--- pages/Resume.py
+++ pages/Resume.py
@@ -9,4 +9,4 @@ first , second = st.columns(2)
 
 with first:
     #st.camera_input(label= "camera")
-    st.image("Resume.img", width = 500   ) # takes a img as the parameter and put it on the screen and make it an obj of "st.image()"
+    st.image("Resume.img", width = 800  ) # takes a img as the parameter and put it on the screen and make it an obj of "st.image()"
